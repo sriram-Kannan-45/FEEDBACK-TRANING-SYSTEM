534ca08d839338a195223081d4f5e1cde0494da6
Mylambigai <mylambigai16@gmail.com>
2026-03-13 19:44:25 +0530

    Commit file

--- traning-feedback-system/src/main/java/com/trainingfeedback/model/Admin.java
+++ traning-feedback-system/src/main/java/com/trainingfeedback/model/Admin.java
@@ -1,5 +1,15 @@
 package com.trainingfeedback.model;
-
+import java.util.*;
 public class Admin {
-
+	private String userName;
+	private String password;
+	private String role;
+	public void display() {
+		System.out.println("Enter user name:");
+		getUserName(userName);
+		System.out.println("Enter user password:");
+		getPassword(password);
+		System.out.println("Enter role:");
+		getRole(role);
+	}
 }
