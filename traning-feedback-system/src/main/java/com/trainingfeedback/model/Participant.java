12c4b53b40dcae891ebb99d5eec9b18f2e5d718d
Tamilarasu143 <tamilarasu143@gmail.com>
2026-03-16 21:09:51 +0530

    first commit

--- traning-feedback-system/src/main/java/com/trainingfeedback/model/Participant.java
+++ traning-feedback-system/src/main/java/com/trainingfeedback/model/Participant.java
@@ -1,6 +1,12 @@
 package com.trainingfeedback.model;
 
+ppackage com.trainingfeedback.model;
+
+import java.util.ArrayList;
+
 public class Participant extends User {
+
+    private ArrayList<String> feedbackList = new ArrayList<>();
 
     public Participant(int id, String name, String password) {
         super(id, name, password);
@@ -9,5 +15,13 @@ public class Participant extends User {
     public void display() {
         System.out.println("Participant ID: " + id);
         System.out.println("Participant Name: " + name);
+    }
+
+    public void addFeedback(String feedback) {
+        feedbackList.add(feedback);
+    }
+
+    public ArrayList<String> getFeedbackList() {
+        return feedbackList;
     }
 }
