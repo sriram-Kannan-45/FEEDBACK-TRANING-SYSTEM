d0ed5e63b7420ed6f6217c6a460f6150cdede887
win <samihamuhabathulla@gmail.com>
2026-03-17 21:45:07 +0530

    Initial commit

---
+++ traning-feedback-system/src/main/java/com/trainingfeedback/Demo/src/sample/TestUnit.java
@@ -0,0 +1,22 @@
+package sample;
+import java.util.Scanner;
+
+public class TestUnit {
+	  public static void main(String[] args) {
+
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter your name: ");
+	        String name = sc.nextLine();
+
+	        System.out.print("Enter your age: ");
+	        int age = sc.nextInt();
+
+	        System.out.println("Your name is: " + name);
+	        System.out.println("Your age is: " + age);
+
+	        sc.close();
+	    }
+	}
+
+
