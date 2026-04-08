85482a8dabbffd1cf3236e4ca281cc690eb2c68f
SRIRAM <wavene20@gmail.com>
2026-04-08 23:46:26 +0530

    Add JDBC documentation and console project from old commit

---
+++ traning-feedback-system/console/src/main/java/com/trainingfeedback/exception/AuthenticationException.java
@@ -0,0 +1,7 @@
+package com.trainingfeedback.exception;
+
+public class AuthenticationException extends RuntimeException {
+    public AuthenticationException(String message) {
+        super(message);
+    }
+}
